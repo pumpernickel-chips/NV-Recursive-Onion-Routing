eb7783c2217be26d6159068877c352edde1b32fa
Naomi <104791815+pumpernickel-chips@users.noreply.github.com>
2023-09-22 14:53:31 -0400

    Merged changes and resolved conflicts

--- src/OnionTester.java
+++ src/OnionTester.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class OnionTester {
+    public OnionTester(){
+    }
     public static void main(String[] args){
         OnionRouter testOnion = new OnionRouter();
+        /*
         System.out.println(testOnion.getInput());
         System.out.println(testOnion.lazyStringMethod(testOnion.getInput()));
+        */
+        Scanner scnr = new Scanner(System.in);
+        int testInput = Integer.parseInt(scnr.nextLine());
 
         ArrayList<Integer> intList = new ArrayList<Integer>(Arrays.asList(1,0,1,0,1));
 
