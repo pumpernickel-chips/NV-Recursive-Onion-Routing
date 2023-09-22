3586869799a9a8d5d30f57520f2f48902906c40e
Naomi <104791815+pumpernickel-chips@users.noreply.github.com>
2023-09-22 14:06:43 -0400

    Added OnionRouter.lazyStringMethod() and OnionRouter.getInput() + tester code

--- src/OnionRouter.java
+++ src/OnionRouter.java
@@ -1,2 +1,12 @@
 public class OnionRouter {
+    private int input;
+    public OnionRouter(){
+        this.input = 0123456;
+    }
+    public int lazyStringMethod(int in){
+        return Integer.parseInt((String.valueOf(in)).substring(1,(String.valueOf(in).length()-1)));
+    }
+    public int getInput(){
+        return this.input;
+    }
 }
--- src/OnionTester.java
+++ src/OnionTester.java
@@ -1,2 +1,7 @@
 public class OnionTester {
+    public static void main(String[] args){
+        OnionRouter testOnion = new OnionRouter();
+        System.out.println(testOnion.getInput());
+        System.out.println(testOnion.lazyStringMethod(testOnion.getInput()));
+    }
 }
