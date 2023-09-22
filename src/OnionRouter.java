e82c3e234d1cf8dd3a58a3e9e5bde6972cf608ab
vserra3192 <vserra3192@gmail.com>
2023-09-22 14:50:19 -0400

    Added OnionRouter.intToBoolToInt; Added OnionRouter.flip; Added some tester code;

--- src/OnionRouter.java
+++ src/OnionRouter.java
@@ -1,12 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class OnionRouter {
+    private List<Boolean> boolArray = new ArrayList<Boolean>();
     private int input;
     public OnionRouter(){
         this.input = 0123456;
     }
+
     public int lazyStringMethod(int in){
         return Integer.parseInt((String.valueOf(in)).substring(1,(String.valueOf(in).length()-1)));
     }
+
     public int getInput(){
         return this.input;
     }
+
+    public List<Boolean> intToBoolToInt(List<Integer> intList) {
+        for (int in: intList) {
+            if (in == 1){
+                boolArray.add(true);
+            }else if(in == 0){
+                boolArray.add(false);
+            }
+        }
+        return boolArray;
+    }
+
+    public List<Boolean> flip(List<Boolean> boolList){
+        List<Boolean> flipBoolean = new ArrayList<>();
+        int i = 0;
+        for (Boolean b: boolList) {
+            i++;
+        }
+        return null;
+    }
+
 }
--- src/OnionTester.java
+++ src/OnionTester.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class OnionTester {
     public static void main(String[] args){
         OnionRouter testOnion = new OnionRouter();
         System.out.println(testOnion.getInput());
         System.out.println(testOnion.lazyStringMethod(testOnion.getInput()));
+
+        ArrayList<Integer> intList = new ArrayList<Integer>(Arrays.asList(1,0,1,0,1));
+
+        System.out.println(testOnion.intToBoolToInt(intList));
+
     }
 }
