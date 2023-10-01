edb51c4e67d29a9325335f0c294103e11e21c6e5
vserra3192 <vserra3192@gmail.com>
2023-09-30 22:34:24 -0400

    Added OnionRouter.toString, OnionRouter.equals, Updated OnionTester.oddBinaryList

--- src/OnionRouter.java
+++ src/OnionRouter.java
@@ -3,11 +3,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class OnionRouter {
     private static Integer step;
     private static Integer length;//the original length of the input
-    private List<Integer> intList = new ArrayList<Integer>();
     private List<Integer> trimmedIntList = new ArrayList<Integer>();
     //private List<Integer> digits;
     private OnionRouter last;
@@ -33,6 +33,7 @@ public class OnionRouter {
             ints.add(0, (trimmedIntList.get(0)==1? 0 : 1));
             ints.add(trimmedIntList.get(1)==1? 0 : 1);
             printDigits(ints);
+            System.out.println(toString()); //ToSTRING TEST
         }else if(ints.size()>1 && step>0){//subtractive process
             trimmedIntList.add(ints.get(0));
             ints.remove(0);
@@ -42,6 +43,7 @@ public class OnionRouter {
             step--;
             printDigits(ints);
             new OnionRouter(ints, this);
+            System.out.println(toString()); //ToSTRING TEST
         }else if(ints.size()>=1 && step<0){//additive process
             ints.add(0, (trimmedIntList.get(0)==1? 0 : 1));
             step--;
@@ -49,11 +51,13 @@ public class OnionRouter {
             step--;
             printDigits(ints);
             this.last.onion(ints);
+            System.out.println(toString()); //ToSTRING TEST
         }else if(ints.size()==1 && step>0){//bit flip
             ints = new ArrayList<Integer>(Arrays.asList(ints.get(0)==1? 0 : 1));
             printDigits(ints);
             step = -3;
             this.last.onion(ints);
+            System.out.println(toString()); //ToSTRING TEST
         }
         return ints;
     }
@@ -62,5 +66,20 @@ public class OnionRouter {
         for(int i = 0; i<((length-list.size())/2); i++) digits += " ";
         for(Integer n : list) digits += n;
         System.out.println(digits);
+    }
+
+    @Override
+    public String toString() {
+        return ("trimmedIntList: " + trimmedIntList + ", step: " + step + ", length: " + length + ", last: " + last);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        OnionRouter other = (OnionRouter) o;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!Objects.equals(trimmedIntList, other.trimmedIntList))
+            return false;
+        return Objects.equals(last, other.last);
     }
 }
