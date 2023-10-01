07f587b51d43452114a8e7754ebee1a311312ab2
Naomi <104791815+pumpernickel-chips@users.noreply.github.com>
2023-10-01 16:36:39 -0400

    Removed redundant OnionRouter methods and attributes including step and size, removed redundant cases in OnionRouter.onionLayer and changed approach, removed the private constructor, simplified OnionRouter.toString() to replace OnionRouter.printDigits, updated equals method, included exception handling, finished OnionRouter comments, and updated tester code

--- src/OnionRouter.java
+++ src/OnionRouter.java
@@ -1,138 +1,77 @@
 //Naomi Coakley & Victor Serra
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class OnionRouter {
-    private static Integer step;
-    /**
-     * <p>The length of the original input given by the user.</p>
-     * <p>This field is static for access by all instances of {@code OnionRouter} within a single recursive process.</p>
-     * <p>This value is set to {@code null} when the base case of {@link #onionLayer} is invoked during the final step
-     * of recursion, indicating that no recursive process is currently running, thus avoiding parallel {@code OnionRouter}
-     * instance conflicts.</p>
-     */
-    private static Integer size;;
-    private List<Integer> outerBits = new ArrayList<Integer>();
     private List<Integer> bits;
-    private OnionRouter last;
-    /**<p>
-     * Public zero args constructor.
+    /**
+     * <p>
+     *     Default zero args constructor.
+     * </p><p>
+     * Passes a default {@code List} of values (representing the binary number {@code 1101011}) to the main constructor.
      * </p>
-     * Passes in an {@code ArrayList<Integer>} as a default test input value representing the binary figure {@code 1101011} to the main public constructor.
      */
-    public OnionRouter(){
-        this(new ArrayList<Integer>(Arrays.asList(1, 1, 0, 1, 0, 1, 1)));
+    public OnionRouter() throws InputMismatchException {
+        this(Arrays.asList(1, 1, 0, 1, 0, 1, 1));
     }
     /**
-     * <p>Main public constructor requiring one parameter.</p>
-     * Initializes {@code OnionRouter.bits} with the {@code input} passed in, sets static fields {@link #step}
-     * and {@link #size} equal to the number of elements in {@code OnionRouter.bits}.
+     * <p>
+     *     Main constructor requiring one parameter.
+     * </p><p>
+     * If no exception is thrown, {@link #bits} is initialized with the {@code input} passed in and the starting value
+     * prints to console. {@link #bits} is then set equal to the return value of {@link #onionLayer}, and this updated
+     * value also prints to console.
+     * </p>
      * @param input a list that must contain an odd number of binary digits
+     * @throws InputMismatchException if {@code input} has an even number of elements
      * */
-    public OnionRouter(List<Integer> input){
+    public OnionRouter(List<Integer> input) throws InputMismatchException {
+        if(input.size()%2==0) throw new InputMismatchException();
         this.bits = input;
-        step = bits.size();
-        size = step;
-        this.printBits(bits);
-        bits = this.onionLayer(bits);
-        System.out.print("*");
-        this.printBits(bits);
-    }
-    /**
-     * <p>Private complete constructor requiring two parameters, called only by {@link #onionLayer} during the recursive process.</p>
-     * The intention of this private constructor is to overload the main constructor starting on the second step of recursion to avoid overwriting the static fields {@code OnionRouter.step} and {@code OnionRouter.inputSize} like the main constructor would.;
-     * @param input a list that must contain an odd number of binary digits
-     * @param last reference to the {@code OnionRouter} instance that invoked this constructor
-     */
-    private OnionRouter(List<Integer> input, OnionRouter last){
-        this.bits = input;
-        this.last = last;
+        System.out.println(this);
         this.bits = this.onionLayer(this.bits);
-        this.printBits(bits);
+        System.out.println(this);
     }
     /**
-     * <p>Recursive method requiring one parameter.</p>
-     * <p>Divided into {@code if} blocks representing the following distinct cases during the recursive process:</p>
-     * <p><i>base case</i>, when {@link #step} is negative with an absolute value equal to {@link #size}, which always
-     * indicates that all but the final step of the recursive process have been executed</p>
-     * <p></p>
-     * <p></p>
-     * <p></p>
+     * <p>
+     *     Recursive method requiring one parameter.
+     * </p><p>
+     *     For any {@code input} with more than one element, the {@code else} statement is executed, initializing a
+     * new {@code OnionRouter} with the same {@code List} as a parameter, but without the first and last elements. The
+     * last two lines in the {@code else} statement do not execute until all lines in the constructor of the new
+     * {@code OnionRouter} object have been executed.
+     * </p><p>
+     *     The base case condition is satisfied only once during the recursive process, at the halfway point when the
+     * {@code input} has been reduced to one element. The single "bit" is flipped and the method is allowed to return.
+     * At this point in the recursive sequence, no more {@code OnionRouter} objects will be initialized and the current
+     * instance will be the first one in its sequence to break out of this {@code if-else} block and return.
+     * </p><p>
+     *     After this, the other instances resume executing the remainder of their methods and constructors in reverse
+     * order, similarly flipping the first and last elements to match the middle element(s) before returning.
+     * </p>
+     * @param input a list that containing an odd number of binary digits
+     * @throws InputMismatchException if input has an even number of elements when passed to a new {@code OnionRouter}
      * */
-    public List<Integer> onionLayer(List<Integer> ints) {
-        //System.out.println("bits @ step " + step + ": " + bits);
-        //base case
-        if(step / size == -1){
-            //System.out.println("ints before add: " + ints);
-            //System.out.println("bits before add: " + bits);
-            ints.add(0, (outerBits.get(0) == 1? 0 : 1));
-            ints.add(outerBits.get(1) == 1? 0 : 1);
-            //step = null;
-            //size = null;
-            //System.out.println("ints before return: " + ints);
-            //System.out.println("bits before return: " + bits);
-
-            return ints;
-        //pre-flip case
-        }else if(ints.size() > 1 && step > 0){
-            outerBits.add(ints.get(0));
-            ints.remove(0);
-            step--;
-            outerBits.add(ints.get(ints.size() - 1));
-            ints.remove(ints.size() - 1);
-            step--;
-            printBits(ints);
-            new OnionRouter(ints, this);
-            System.out.print("*");
-            printBits(ints);
-            //System.out.println("pre-flip case breaks at step " + step);
-        //bit-flip case
-        }else if(ints.size() == 1 && step > 0){
-            step--;
-            printBits(ints);
-            ints = new ArrayList<Integer>(Arrays.asList(ints.get(0) == 1? 0 : 1));
-            printBits(ints);
-            step--;
-            return this.last.onionLayer(ints);
-        //post-flip case
-        }else if(ints.size() >= 1 && step < 0){
-            ints.add(0, (outerBits.get(0) == 1? 0 : 1));
-            step--;
-            ints.add(outerBits.get(1) == 1? 0 : 1);
-            step--;
-            printBits(ints);
-            return this.last.onionLayer(ints);
+    private List<Integer> onionLayer(List<Integer> input) throws InputMismatchException {
+        if(input.size() == 1){//base case:
+            input.set(0, (input.get(0) == 1? 0 : 1));
+        }else{//all other cases:
+            new OnionRouter(input.subList(1, input.size() - 1));
+            input.set(0, (input.get(0) == 1? 0 : 1));
+            input.set(input.size() - 1, (input.get(input.size() - 1) == 1? 0 : 1));
         }
-        //System.out.println("entire method returns at step " + step);
-        return ints;
+        return input;
     }
-    public void printBits(List<Integer> list) {
-        String out = "";
-        if(size != null){
-            for(int i = 0; i < ((size - list.size()) / 2); i++) out += " ";
-        }
-        for(Integer n : list) out += n;
-        System.out.println(out);
-    }
-    public static boolean isBusy(){
-        return step == null && size == null;
-    }
-
     @Override
     public String toString() {
-        return ("trimmedIntList: " + outerBits + ", step: " + step + ", length: " + size + ", last: " + last);
+        return (this.bits.stream().map(Object::toString).collect(Collectors.joining("")));
     }
-
     @Override
     public boolean equals(Object o) {
         OnionRouter other = (OnionRouter) o;
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        if (!Objects.equals(outerBits, other.outerBits))
-            return false;
-        return Objects.equals(last, other.last);
+        return Objects.equals(bits, other.bits);
     }
 }
--- src/OnionTester.java
+++ src/OnionTester.java
@@ -1,11 +1,10 @@
 //Naomi Coakley & Victor Serra
 
-import java.io.IOException;
 import java.util.*;
 public class OnionTester {
     public OnionTester(){
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InputMismatchException {
         OnionTester test = new OnionTester();
         Scanner scnr = new Scanner(System.in);
         List<Integer> intList;
@@ -13,30 +12,31 @@ public class OnionTester {
         while(true) {
             try {
                 System.out.print("Enter Value: ");
-                intList = test.oddBinaryList(scnr);/*new ArrayList<Integer>(Arrays.asList(1,1,0,1,0,1,1))*/
+                intList = test.oddBinaryList(scnr);
                 if(intList.contains(-1)) break;
-                new OnionRouter(intList);
-            } catch (IOException e) {
-                System.out.println("Input must be an odd number of binary digits. \nEnter \"e\" to Exit");
+                System.out.println();
+                OnionRouter onion = new OnionRouter(intList);
+                System.out.println("\nEnter 'e' to Exit");
+            } catch (InputMismatchException e) {
+                System.out.println("Input must be an odd number of binary digits.\nEnter \"e\" to Exit");
             }
         }
     }
-    public List<Integer> oddBinaryList(Scanner s) throws IOException {
+    public List<Integer> oddBinaryList(Scanner s) throws InputMismatchException {
         List<Integer> list = new ArrayList<Integer>();
             char[] testInput = ("" + s.nextLine()).toCharArray();
             for(char c : testInput){
-                if(c=='0'){
+                if(c=='e'){
+                    return Collections.singletonList(-1);
+                }else if(c=='0'){
                     list.add(0);
                 }else if(c=='1'){
                     list.add(1);
-                }else if(c=='e'){
-                    list.add(-1);
                 }else{
-                    throw new IOException();
+                    throw new InputMismatchException();
                 }
             }
-            if(list.size()%2==0) throw new IOException();
-            //System.out.println(testInput);
+        if(list.size()%2==0) throw new InputMismatchException();
         return list;
     }
 }
